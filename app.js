f2f8e852663e656a5a277dfce927c7b8b2abef67
Jawwad <jawwadrehan96@outlook.com>
2021-07-03 19:25:36 +0530

    func: added function for translation

--- app.js
+++ app.js
@@ -8,5 +8,21 @@ function getTranslationUrl(text) {
     return serverURL + "?" + "text=" + text; 
 }
 
+function handlingErrors(error) {
+    alert("Oops somethings is wrong, try after sometime", error)
+}
 
+function clickEventHandler() {
+    var inputText = txtInput.value;
+
+    fetch(getTranslationUrl(inputText))
+        .then(response => response.json())
+        .then(json => {
+           var translatedText = json.contents.translated;
+           txtOutput.innerText = translatedText;
+        })
+        .catch(handlingErrors);
+}
+
+btnTranslate.addEventListener("click", clickEventHandler())
 
